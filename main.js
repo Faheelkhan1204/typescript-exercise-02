591affd60944a2fb0d3c5e55ca5d801214c8851f
Faheelkhan1204 <153633614+Faheelkhan1204@users.noreply.github.com>
2024-03-14 09:39:41 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+"use strict";
+// Arithmetic Operators
+// let num1 = 2;
+// let num2 = 4;
+// console.log(num1 + num2);
+// let num1=5
+// let num2=5
+// console.log(num1-num2)
+// let num1=5
+// let num2=5
+// console.log(num1*num2)
+// let num1=5
+// let num2=5
+// console.log(num1**num2)
+let num1=5
+let num2=2
+console.log(num1%num2)
---
+++ main.ts
@@ -0,0 +1,4 @@
+// Arithmetic Operators
+let num1=2
+let num2=4
+console.log(num1+num2)
